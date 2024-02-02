a9995ac9240f00a47581d9397029ec875d4fe5e4
Martin <martymalkav7@gmail.com>
2024-02-01 20:58:46 -0800

    4. Tasks

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" a function that takes in an int argument named
+that waits for a random delay between 0 and 10
+seconds and eventually returns it.
+"""
+import random
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """ Prints a list of delays in ascending order. """
+    task = [await task_wait_random(max_delay) for i in range(n)]
+    return sorted(task)
